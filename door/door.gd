5fff5b173f4c7fad482a626098c9b41210458f0f
Milan Vaško <m.vasko2@gmail.com>
2024-04-14 17:31:54 +0200

    Door sounds

--- door/door.gd
+++ door/door.gd
@@ -6,8 +6,13 @@ extends AnimatableBody3D
 @export var time_to_change: float
 var current_time_to_change: float
 
+@export var open_sound_clips: Array[AudioStream]
+@export var close_sound_clips: Array[AudioStream]
+
 var previous_rotation_degrees: Vector3
 var target_rotation_degrees: Vector3
+
+@onready var audio_player := $AudioStreamPlayer3D
 
 func _ready() -> void:
 	rotation_degrees = Vector3.UP * (opened_angle if opened else closed_angle)
@@ -21,6 +26,10 @@ func _can_interact() -> bool:
 func _interact() -> void:
 	previous_rotation_degrees = Vector3.UP * (opened_angle if opened else closed_angle)
 	opened = !opened
+	if audio_player.playing:
+		audio_player.stop()
+	audio_player.stream = (open_sound_clips if opened else close_sound_clips).pick_random()
+	audio_player.play()
 	target_rotation_degrees = Vector3.UP * (opened_angle if opened else closed_angle)
 	current_time_to_change = 0.0
 
