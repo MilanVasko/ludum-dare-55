2973e66918a04cd537e9114ce765b638f38099e3
Milan Vaško <m.vasko2@gmail.com>
2024-04-14 14:44:32 +0200

    Fish-o-mat sound

--- fishomat/fishomat.gd
+++ fishomat/fishomat.gd
@@ -1,10 +1,15 @@
 extends CharacterBody3D
 
+@onready var audio_player := $AudioStreamPlayer3D
+
 func _interact() -> void:
 	var inventory := Global.require_inventory()
 	if inventory.has_coin():
+		if audio_player.playing:
+			audio_player.stop()
 		inventory.remove_coin()
 		Global.require_dialogue().play_dialogue_from_start("Fish")
+		audio_player.play()
 		inventory.add_fish()
 	else:
 		Global.require_dialogue().play_dialogue_from_start("FishOMat")
